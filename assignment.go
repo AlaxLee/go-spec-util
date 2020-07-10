c4b2c87a3f6d9e531644e157c44323423cd19d32
alax <windsteering@gmail.com>
2020-07-10 11:11:09 +0800

    put assignment's hack in hack.go

--- assignment.go
+++ assignment.go
@@ -1,33 +1,5 @@
 package gospec
 
-import (
-	forceexport "github.com/AlaxLee/go-forceexport"
-	"go/ast"
-	"go/constant"
-	"go/types"
-	"log"
-	"runtime"
-)
-
-//same as method (*Checker).assignment in src/go/types/assignments.go
-var _assignment func(checker *types.Checker, x *operand, T types.Type, context string)
-
-func init() {
-	if runtime.Version() != "go1.14" {
-		log.Println("warning: version is not go1.14, may be panic in usring")
-	}
-
-	// 先触发一次 go/types.(*Checker).assignment 方法，以保证在 runtime.firstmoduledata 中能查到它
-	// 如果查找不到，会报错：panic: Invalid function name: go/types.(*Checker).assignment
-	NewSpec("")
-
-	// 将 _assignment 映射为 go/types.(*Checker).assignment
-	err := forceexport.GetFunc(&_assignment, "go/types.(*Checker).assignment")
-	if err != nil {
-		panic(err)
-	}
-}
-
 func (s *Spec) Assignment(v, t string) bool {
 	V := s.MustGetValidType(v)
 	T := s.MustGetValidType(t)
@@ -44,16 +16,3 @@ func Assignment(code, v, t string) bool {
 	s := NewSpec(code)
 	return s.Assignment(v, t)
 }
-
-//same as type operand in src/go/types/operand.go
-type operand struct {
-	mode operandMode
-	expr ast.Expr
-	typ  types.Type
-	val  constant.Value
-	id   builtinId
-}
-type operandMode byte
-type builtinId int
-
-const value operandMode = 7
