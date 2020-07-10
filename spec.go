2c382bae55545427ebac89e8322b5eef22a0a2ff
alax <windsteering@gmail.com>
2020-07-10 15:39:46 +0800

    add field SearchKind in Spec

--- spec.go
+++ spec.go
@@ -14,11 +14,20 @@ func init() {
 
 var spec *Spec
 
+type SearchKind int
+
+const (
+	OnlyPackage = iota
+	PackageAndUniverse
+	All
+)
+
 type Spec struct {
 	code    string
 	file    *ast.File
 	pkg     *types.Package
 	checker *types.Checker
+	SearchKind
 }
 
 func NewSpec(code string) *Spec {
@@ -37,6 +46,7 @@ func NewSpec(code string) *Spec {
 	c.Error = func(err error) {} // 防止触发 go/types.(*Checker).err 方法里的 panic
 	s.pkg = types.NewPackage(packageName, "")
 	s.checker = types.NewChecker(c, fset, s.pkg, nil)
+	s.SearchKind = PackageAndUniverse // default search in universe and pkg scope
 
 	// 此方法会触发一次 go/types.(*Checker).assignment 方法，以保证在 runtime.firstmoduledata 中能查到它
 	err = s.checker.Files([]*ast.File{s.file})
@@ -47,8 +57,25 @@ func NewSpec(code string) *Spec {
 }
 
 func (s *Spec) GetTypeObject(v string) types.Object {
-	//return lookupByBFS(s.pkg.Scope(), v)
-	return s.pkg.Scope().Lookup(v)
+	switch s.SearchKind {
+	case OnlyPackage:
+		return s.pkg.Scope().Lookup(v)
+	case PackageAndUniverse:
+		o := s.pkg.Scope().Lookup(v)
+		if o == nil {
+			o = types.Universe.Lookup(v)
+		}
+		return o
+	case All:
+		o := lookupByBFS(s.pkg.Scope(), v)
+		if o == nil {
+			o = types.Universe.Lookup(v)
+		}
+		return o
+	default:
+		panic("unexpect")
+	}
+	return nil
 }
 
 func (s *Spec) MustGetValidTypeObject(v string) types.Object {
@@ -94,6 +121,26 @@ func (s *Spec) GetBaseType(v string) types.Type {
 		return nil
 	}
 	return p.Elem()
+}
+
+func (s *Spec) IsInUniverse(v string) bool {
+	switch s.SearchKind {
+	case OnlyPackage, PackageAndUniverse:
+		if s.pkg.Scope().Lookup(v) == nil {
+			if types.Universe.Lookup(v) != nil {
+				return true
+			}
+		}
+	case All:
+		if lookupByBFS(s.pkg.Scope(), v) == nil {
+			if types.Universe.Lookup(v) != nil {
+				return true
+			}
+		}
+	default:
+		panic("unexpect")
+	}
+	return false
 }
 
 func GetTypeObject(code, v string) types.Object {
